015aec8c786f2b2d58dcc3ee804ffc54c7dea0fa
rodolpho-rhaniery <60471524+r-rhaniery@users.noreply.github.com>
2022-04-11 23:41:09 -0300

    mask cep

--- ProjetoEndereco_ComCSS/enderecoFormulario.js
+++ ProjetoEndereco_ComCSS/enderecoFormulario.js
@@ -1,24 +1,34 @@
-function buscarEndereco(){
+function buscarEndereco() {
+  let cep = document.querySelector('#cep').value
 
-        let cep = document.querySelector('#cep').value
+  let url = `https://viacep.com.br/ws/${cep}/json/`
 
-        let url = `https://viacep.com.br/ws/${cep}/json/`
+  fetch(url).then(function (response) {
+    response.json().then(mostrarEndereco)
+  })
+}
 
-        fetch(url).then(function(response){
-            response.json().then(mostrarEndereco)  
-        })
-} 
-
-function mostrarEndereco(dados){
-    let resultado = document.querySelector('#resultado')
-    if (dados.erro) {
-        resultado.innerHTML = 'Não foi possível localizar o endereço!'
-    } else {   
+function mostrarEndereco(dados) {
+  let resultado = document.querySelector('#resultado')
+  if (dados.erro) {
+    resultado.innerHTML = 'Não foi possível localizar o endereço!'
+  } else {
     resultado.innerHTML = `<p>Logradouro: ${dados.logradouro}</p>
                            <p>Complemento: ${dados.complemento}</p> 
                            <p>Bairro: ${dados.bairro}</p>
                            <p>Cidade: ${dados.localidade}</p>
                            <p>Estado: ${dados.uf}</p>`
-
-    }
+  }
 }
+
+const inputValue = document.querySelector('#cep')
+let zipCode = ''
+
+inputValue.addEventListener('keyup', () => {
+  zipCode = inputValue.value
+  if (zipCode)
+    if (zipCode.length === 8) {
+      inputValue.value = `${zipCode.substr(0, 5)}-${zipCode.substr(5, 9)}`
+      console.log(zipCode)
+    }
+})
